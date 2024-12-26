24a4a215001b20cdfe3b6e51c7139de4d277121c
Swittee Franca <switteefrankie@gmail.com>
2024-12-26 21:11:26 +0100

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -10,13 +10,6 @@ admin.initializeApp({
 })
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB0xMcnwwj7qlZAzDL-UbH8pLBctXVwUcU',
-  authDomain: 'test-d764f.firebaseapp.com',
-  projectId: 'test-d764f',
-  storageBucket: 'test-d764f.firebasestorage.app',
-  messagingSenderId: '509210843981',
-  appId: '1:509210843981:web:e9db32b667bf0ab651b504',
-  measurementId: 'G-163HH48S44',
 }
 
 // Initialize Firebase
